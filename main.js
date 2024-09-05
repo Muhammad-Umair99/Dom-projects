480a197451ebf263d864c7ed9c22dca8d42d9b51
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-05 16:00:40 +0500

    add & remove friend

---
+++ main.js
@@ -0,0 +1,43 @@
+//FIRST TASK
+
+//two button with add and remove
+
+// let istatus = document.querySelector("h5")
+
+// let add = document.querySelector("#add")
+// let remove = document.querySelector("#remove")
+
+
+// add.addEventListener("click",()=>{
+//     istatus.innerText = "Friends"
+//     istatus.style.color = "green"
+// })
+// remove.addEventListener("click",()=>{
+//     istatus.innerText = "Stranger"
+//     istatus.style.color = "red"
+// })
+
+ 
+
+//one button with add and remove
+
+// let istatus = document.querySelector("h5")
+
+// let btn = document.querySelector("#add")
+
+// let val = "stranger"
+
+// btn.addEventListener("click", () => {
+//     if (val === "stranger") {
+//         istatus.innerHTML = "Friends"
+//         istatus.style.color = "green"
+//         btn.innerHTML   ="Remove Friend"
+//         val = "friend"
+//     }
+//     else {
+//         istatus.innerHTML = "Stranger"
+//         istatus.style.color = "red"
+//          btn.innerHTML   ="Add Friend"
+//         val = "stranger"
+//     }
+// })
