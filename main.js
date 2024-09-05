f2fb255956e12cf8a31e094eb879ffc679d453aa
Muhammad-Umair99 <muhammadumair10010@gmail.com>
2024-09-05 18:36:58 +0500

    click & show heart

--- main.js
+++ main.js
@@ -17,7 +17,7 @@
 //     istatus.style.color = "red"
 // })
 
- 
+
 
 //one button with add and remove
 
@@ -41,3 +41,21 @@
 //         val = "stranger"
 //     }
 // })
+
+
+
+/* SECOND TASK */
+// let con = document.querySelector("#container")
+// let love = document.querySelector("i")
+
+// con.addEventListener("dblclick", () => {
+//     love.style.transform = "translate(-50%,-50%)scale(1)"
+//     love.style.opacity = 0.8
+//     setTimeout(() => {
+//         love.style.opacity = 0
+//     }, 1000)
+//     setTimeout(() => {
+//         love.style.transform = "translate(-50%,-50%)scale(0)"
+//     }, 2000)
+// })
+
